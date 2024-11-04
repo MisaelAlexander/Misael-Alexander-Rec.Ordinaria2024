1d02cf3b7cf4dcac287c1952e9296c5ddd2532bc
gvze7g <20230633@ricaldone.edu.sv>
2024-11-04 14:46:25 -0600

    si

---
+++ Model/DAO/DAODocentes.cs
@@ -0,0 +1,120 @@
+﻿using Refuerzo2024.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refuerzo2024.Model.DAO
+{
+    internal class DAODocentes : DTODocentes
+    {
+        SqlConnection con = obtenerConexion();
+        public DataSet Docentes()
+        {
+            try
+            {
+                string query = "Select*From ViewDocentes";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "ViewDocentes");
+                return ds;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public bool Agregar()
+        {
+            try
+            {
+                string query = "Insert Into Docentes Values @param1,@param2,@param3";
+                SqlCommand cmdInsert = new SqlCommand(query, con);
+                cmdInsert.Parameters.AddWithValue("param1",NombreDocente);
+                cmdInsert.Parameters.AddWithValue("param2",ApellidoDocente);
+                cmdInsert.Parameters.AddWithValue("param3",DUI1);
+                cmdInsert.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public bool Actualizar()
+        {
+            try
+            {
+                string query = "Update Docentes SET nombreDocente = @param1, apellidoDocente = @param2, dui = @param3 Where idDocente = @param0";
+                SqlCommand cmdInsert = new SqlCommand(query, con);
+                cmdInsert.Parameters.AddWithValue("param1", NombreDocente);
+                cmdInsert.Parameters.AddWithValue("param2", ApellidoDocente);
+                cmdInsert.Parameters.AddWithValue("param3", DUI1);
+                cmdInsert.Parameters.AddWithValue("param0", IdDocente);
+                cmdInsert.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public bool Eliminar()
+        {
+            try
+            {
+                string query = "Delete From Docentes  Where idDocente = @param0";
+                SqlCommand cmdInsert = new SqlCommand(query, con);
+                cmdInsert.Parameters.AddWithValue("param0", IdDocente);
+                cmdInsert.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public DataSet BuscarDocentes(string Valor)
+        {
+            try
+            {
+                string query = $"Select*From ViewDocentes Where ID = '%{Valor}%' OR Nombre OR Apellido ";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "ViewDocentes");
+                return ds;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }   
+}
---
+++ Model/DTO/DTODocentes.cs
@@ -0,0 +1,22 @@
+﻿using Refuerzo2024.Model.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refuerzo2024.Model.DTO
+{
+    internal class DTODocentes:dbContext
+    {
+        int idDocente;
+        string nombreDocente;
+        string apellidoDocente;
+        string DUI;
+
+        public int IdDocente { get => idDocente; set => idDocente = value; }
+        public string NombreDocente { get => nombreDocente; set => nombreDocente = value; }
+        public string ApellidoDocente { get => apellidoDocente; set => apellidoDocente = value; }
+        public string DUI1 { get => DUI; set => DUI = value; }
+    }
+}
